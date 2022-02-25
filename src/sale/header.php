ccb2cd5d74be488c8031834b9f8356c99eb6ce7b
cana <canacana2@gmail.com>
2022-02-25 17:51:54 +0900

    scssの導入

--- src/sale/header.php
+++ src/sale/header.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="stylesheets/css/app.css">
+    <link href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet">
 </head>
+
 <body>
-    <h1>テストテストテスト</h1>
-    <p>テスト</p>
+    
 </body>
+
 </html>
