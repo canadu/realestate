f70a33a7bcafbdae493192c1c317c6501d5e43a3
cana <canacana2@gmail.com>
2022-04-04 17:28:32 +0900

    0404

--- src/estate/auth/dashboard.php
+++ src/estate/auth/dashboard.php
@@ -1,10 +1,31 @@
 <?php
 session_start();
 require '../db/dbConnect.php';
+
+if (empty($_SESSION['username'])) {
+    //ログインしていない場合はログイン画面に遷移する
+    header('Location: login.php');
+}
+$db = new DataSource;
+if ($_SESSION['role'] == 'admin') {
+    //ユーザーの登録人数を取得
+    $sql = "SELECT count(*) as register_user FROM users";
+    $count = $db->select($sql, []);
+    //物件の数を取得
+    $total_rent = $db->select('SELECT count(*) as total_rent FROM room_rental_registrations');
+    //物件の数(アパート)を取得
+    $total_rent_apartment = $db->select('SELECT count(*) as total_rent_apartment FROM room_rental_registrations_apartment');
+}
+$sql = "SELECT count(*) as total_auth_user_rent FROM room_rental_registrations WHERE user_id = :user_id";
+$total_auth_user_rent = $db->selectOne($sql, [':user_id' => $_SESSION['id']]);
+
+$sql = "SELECT count(*) as total_auth_user_rent_ap FROM room_rental_registrations_apartment WHERE user_id = :user_id";
+$total_auth_user_rent_ap = $db->selectOne($sql, [':user_id' => $_SESSION['id']]);
+
 include '../include/header.php';
-include '../include/side-nav.php';
+
 ?>
-<!-- nav -->
+
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#212529;" id="mainNav">
     <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="../index.php">SHRS</a>
@@ -15,9 +36,9 @@ include '../include/side-nav.php';
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav text-uppercase ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><?= $_SESSION['fullname'] ?><?php if ($_SESSION['role'] == 'admin') {
-                                                                                    echo "(Admin)";
-                                                                                } ?>
+                    <a class="nav-link" href="#"><?php echo $_SESSION['fullname']; ?> <?php if ($_SESSION['role'] == 'admin') {
+                                                                                            echo "(Admin)";
+                                                                                        } ?></a>
                 </li>
                 <li class="nav-item">
                     <a href="logout.php" class="nav-link">Logout</a>
@@ -26,6 +47,9 @@ include '../include/side-nav.php';
         </div>
     </div>
 </nav>
+
+<?php include '../include/side-nav.php'; ?>
+
 <section class="wrapper" style="margin-left:16%;margin-top:-11%;">
     <div class="col-md-12">
         <h1>Dashboard</h1>
@@ -38,7 +62,7 @@ include '../include/side-nav.php';
                             <a href="../app/users.php">
                                 <div class="card-counter primary">
                                     <i class="fa fa-user"></i>
-                                    <span class="count-numbers">'<?php $count['register_user']; ?>'</span>
+                                    <span class="count-numbers"><?php echo $count[0]['register_user']; ?></span>
                                     <span class="count-name">Users</span>
                                 </div>
                             </a>
@@ -47,7 +71,7 @@ include '../include/side-nav.php';
                             <a href="../app/list.php">
                                 <div class="card-counter danger">
                                     <i class="fa fa-home"></i>
-                                    <span class="count-numbers">'<?php (intval($total_rent['total_rent']) + intval($total_rent_apartment['total_rent_apartment'])) ?>'</span>
+                                    <span class="count-numbers"><?php echo (intval($total_rent[0]['total_rent'])) + (intval($total_rent_apartment[0]['total_rent_apartment'])); ?></span>
                                     <span class="count-name">Rentals</span>
                                 </div>
                             </a>
@@ -62,7 +86,7 @@ include '../include/side-nav.php';
                             <a href="../app/list.php">
                                 <div class="card-counter success">
                                     <i class="fa fa-home"></i>
-                                    <span class="count-numbers">'<?php $total_auth_user_rent['total_auth_user_rent']; ?>'</span>
+                                    <span class="count-numbers">'<?php $total_auth_user_rent[0]['total_auth_user_rent']; ?>'</span>
                                     <span class="count-name">Registered Rooms</span>
                                 </div>
                             </a>
@@ -73,7 +97,7 @@ include '../include/side-nav.php';
         </div>
     </div>
 </section>
-</style>
+
 <style>
     .card-counter {
         box-shadow: 2px 2px 10px #DADADA;
@@ -133,3 +157,4 @@ include '../include/side-nav.php';
         font-size: 18px;
     }
 </style>
+<?php include '../include/footer.php'; ?>
--- src/estate/include/side-nav.php
+++ src/estate/include/side-nav.php
@@ -1,33 +1,38 @@
 <?php
-if (empty($_SESSION['role'])){
+if (empty($_SESSION['role'])) {
+    //権限が空の場合はログイン画面へ遷移
     header('Location: login.php');
 }
 ?>
-<nav style="background-color:#212529;" id="mainNav" class="navbar navbar-expand-sm navbar-default sidebar">
+<br>
+<nav class="navbar navbar-expand-sm navbar-default sidebar" style="background-color:#212529;" id="mainNav">
     <div class="container">
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fa fa-bars"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive1">
-            <ul class="navbar-nav text-center" style="    flex-direction: column;">
+            <ul class="navbar-nav text-center" style="flex-direction:column;">
                 <li class="nav-item">
                     <a class="nav-link" href="../auth/dashboard.php">Home</a>
                 </li>
+                <!-- 登録 -->
                 <li class="nav-item">
-                    <?php if ($_SESSION['role'] == 'admin' || $_SESSION['user']) {
+                    <?php if ($_SESSION['role'] == 'admin' || $_SESSION['role'] == 'user') {
                         echo '<a href="../app/register.php" class="nav-link">Register</a>';
-                    }
-                    ?>
+                    } ?>
                 </li>
+                <!-- リスト -->
                 <li class="nav-item">
                     <a href="../app/list.php" class="nav-link">Details/Update</a>
                 </li>
+                <!-- SMS送信画面 -->
                 <li class="nav-item">
                     <?php if ($_SESSION['role'] == 'admin') {
                         echo '<a href="../app/sms.php" class="nav-link">Send SMS</a>';
                     } ?>
                 </li>
+                <!-- 苦情リスト -->
                 <li class="nav-item">
                     <?php if ($_SESSION['role'] == 'admin') {
                         echo '<a href="../app/cmplist.php" class="nav-link">Complaint List</a>';
